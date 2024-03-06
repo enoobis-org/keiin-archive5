e8ea6a06b1e8f286315d1320e529577a0ab62255
enoobis <62465404+enoobis@users.noreply.github.com>
2024-03-06 10:46:01 +0600

    Initial commit

---
+++ C++/1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age;
+    cout << "How old are you?\n";
+    cin >> age    ;
+    cout << "You are " << a << " years old";
+
+    return 0;
+}
